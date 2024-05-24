9181838dfb169812516cb811182a1e75f24f39e4
XUM <xugartemeso@gmail.com>
2024-05-24 11:17:36 +0200

    primer commit

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
+const Server = require('./models/server');
+
 require('dotenv').config();
-const express = require('express');
 
-const port = process.env.PORT;
-const app = express();
+const server = new Server();
 
-//creamos un middleware que diga cual es el directorio principal de la página estática
-//los middleware se ejecutan antes del resto del contenido
-
-app.use(express.static('public'));
-
-//sendFile para enviar un archivo
-app.get('/contacta',(req,res)=>{
-    res.sendFile(`${__dirname}/public/contacta.html`);
-});
-app.get('/localizacion',(req,res)=>{
-    res.sendFile(`${__dirname}/public/localizacion.html`);
-});
-app.get('/*',(req,res)=>{
-    res.sendFile(`${__dirname}/public/404.html`);
-});
-
-app.listen(port,()=>{console.log('http://localhost:8080')})
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+class Server{
+    constructor() {
+        this.port = process.env.PORT;
+        this.app = express();
+        this.middleware();
+        this.routes();
+    }
+
+    middleware() {
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        this.app.use('/contacta', (req,res)=> {
+            res.sendFile(path.resolve('public/contacta.html'));   
+        });
+        this.app.use('/localizacion', (req,res)=> {
+            res.sendFile(path.resolve('public/localizacion.html'));   
+        });
+        this.app.use('*', (req,res)=> {
+            res.sendFile(path.resolve('public/404.html'));   
+        });
+    }
+    listen() {
+        this.app.listen(this.port, () => {console.log('http://localhost:8080') });
+    }
+}
+
+
+
+
+module.exports = Server;
